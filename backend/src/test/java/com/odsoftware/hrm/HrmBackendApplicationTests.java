96a487d0a3196465f11e900ab20cb2fad13df28e
ottavio.lucifero@igesa.it <ottavio.lucifero@igesa.it>
2026-05-01 14:48:02 +0100

    TASK-002 create Spring Boot backend skeleton

---
+++ backend/src/test/java/com/odsoftware/hrm/HrmBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.odsoftware.hrm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrmBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
